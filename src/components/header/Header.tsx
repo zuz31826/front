30bbf8927040166596278217fc78d177463b4b0b
DemakovAndrii <demakov1155@gmail.com>
2025-10-21 20:54:16 +0300

    add sidebar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,6 +6,7 @@ import "./header.css";
 const Header: React.FC = () => {
 	const [show, setShow] = useState(true);
 	const [lastScrollY, setLastScrollY] = useState(0);
+	const [menuOpen, setMenuOpen] = useState(false);
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -21,11 +22,44 @@ const Header: React.FC = () => {
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, [lastScrollY]);
 
+	useEffect(() => {
+		if (menuOpen) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "auto";
+		}
+	}, [menuOpen]);
+
 	return (
-		<div className={`header ${show ? "header--visible" : "header--hidden"}`}>
-			<img src={name} alt="Name" className="headerNameImg" />
-			<img src={burger} alt="Burger" className="headerBurgerImg" />
-		</div>
+		<>
+			<div className={`header ${show ? "header--visible" : "header--hidden"}`}>
+				<img src={name} alt="Name" className="headerNameImg" />
+				<img
+					src={burger}
+					alt="Menu"
+					className="headerBurgerImg"
+					onClick={() => setMenuOpen(true)}
+				/>
+			</div>
+
+			<div
+				className={`overlay ${menuOpen ? "overlay--visible" : ""}`}
+				onClick={() => setMenuOpen(false)}
+			/>
+
+			<div className={`sidebar ${menuOpen ? "sidebar--open" : ""}`}>
+				<button className="sidebar__close" onClick={() => setMenuOpen(false)}>
+					×
+				</button>
+				<ul className="sidebar__menu">
+					<li>Tattoo work</li>
+					<li>Visual art</li>
+					<li>Collaboration</li>
+					<li>About me</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+		</>
 	);
 };
 
